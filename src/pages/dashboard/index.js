117cea2ffc7fff3494cf9dfdadd971d7f0d2bb9c
Rashidqf <166512114+Rashidqf@users.noreply.github.com>
2024-06-26 21:25:20 +0500

    ...

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -9,15 +9,27 @@ const index = () => {
     <>
       <Head>
         <title>Next.js App with CDN</title>
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css"  rel="stylesheet" />
+        <link
+          href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css"
+          rel="stylesheet"
+        />
 
-        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+        <script
+          async
+          src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        ></script>
+        <script
+          async
+          src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"
+        ></script>
+        <script
+          async
+          src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
+        ></script>
       </Head>
-      <Header/>
-      <Sidebar/>
-      <Main/>
+      <Header />
+      <Sidebar />
+      <Main />
     </>
   );
 };
