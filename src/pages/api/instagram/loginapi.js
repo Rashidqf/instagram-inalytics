f3033823959a1701f492da9b62cdf19b5a337b44
Rashidqf <166512114+Rashidqf@users.noreply.github.com>
2024-07-09 17:16:36 +0500

    ...

--- src/pages/api/instagram/loginapi.js
+++ src/pages/api/instagram/loginapi.js
@@ -7,9 +7,9 @@ export default async function handler(req, res) {
   await dbConnect();
   console.log("req.body", req.body);
 
-  if (req.method !== "POST") {
-    return res.status(405).json({ error: "Method not allowed" });
-  }
+  // if (req.method !== "POST") {
+  //   return res.status(405).json({ error: "Method not allowed" });
+  // }
 
   const { code } = req.body;
   console.log("Received code:", code);
